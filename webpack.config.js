4661a00f644644c824062cc00bfdc95df0cd9499
EH1984 <eugene_hamill@yahoo.ie>
2020-07-17 16:34:23 +0100

    first_commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      }
+    ]
+  }
+};
